7542b41952f82634055bd850e787607e3dba45a5
何琦 <>
2022-12-17 23:53:00 +0800

    源码：eip-1167，Clones.sol

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -16,17 +16,25 @@ pragma solidity ^0.8.0;
  *
  * _Available since v3.4._
  */
+
+// https://eips.ethereum.org/EIPS/eip-1167
+// EIP 1167是部署最小代理合约的标准，也称为“克隆”。
+// 目的是为了以不可变的方式简单且廉价地克隆合约功能，该标准指定了一个最小字节码的实现，将所有调用委托给一个已知的固定地址。
+
 library Clones {
     /**
      * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
      *
      * This function uses the create opcode, which should never revert.
      */
+    // 部署并返回克隆 implementation 的地址。此函数使用 create 操作码 
     function clone(address implementation) internal returns (address instance) {
         /// @solidity memory-safe-assembly
         assembly {
             // Cleans the upper 96 bits of the `implementation` word, then packs the first 3 bytes
             // of the `implementation` address with the bytecode before the address.
+            // 索引 10 - 29（含）的字节被替换为主功能合约的 20 字节地址
+
             mstore(0x00, or(shr(0xe8, shl(0x60, implementation)), 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000))
             // Packs the remaining 17 bytes of `implementation` with the bytecode after the address.
             mstore(0x20, or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3))
@@ -42,6 +50,8 @@ library Clones {
      * the clone. Using the same `implementation` and `salt` multiple time will revert, since
      * the clones cannot be deployed twice at the same address.
      */
+    // 此函数使用 create2 操作码和 salt来确定性地部署克隆。
+    // 使用相同implementation和salt多次将会revert，因为克隆不能在同一地址部署两次。 
     function cloneDeterministic(address implementation, bytes32 salt) internal returns (address instance) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -58,6 +68,7 @@ library Clones {
     /**
      * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
      */
+    // 可以提前计算使用 cloneDeterministic 部署克隆的地址 
     function predictDeterministicAddress(
         address implementation,
         bytes32 salt,
