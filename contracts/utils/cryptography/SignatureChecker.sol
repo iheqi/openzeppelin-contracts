2e44395c60f78e9eddd2f30f7270a7dca5550048
qihe <>
2022-12-19 21:17:11 +0800

    源码：SignatureChecker.sol，EIP-1271：合约的标准签名验证方法

--- contracts/utils/cryptography/SignatureChecker.sol
+++ contracts/utils/cryptography/SignatureChecker.sol
@@ -21,16 +21,32 @@ library SignatureChecker {
      * NOTE: Unlike ECDSA signatures, contract signatures are revocable, and the outcome of this function can thus
      * change through time. It could return true at block N and false at block N+1 (or the opposite).
      */
+
+    // https://eips.ethereum.org/EIPS/eip-1271
+    // EOA 有私钥进行签署消息，但合约没有私钥。为此给合约制定一种标准来验证代表给定合约的签名是否有效。(代表是该是合约签署的签名)
+    // 这可以通过在签名合约上实现一个isValidSignature(hash, signature)函数来实现，可以调用该函数来验证签名。
+
+    // 验证 signature 对于给定的 signer 和 数据hash 是否有效。如果签名者是智能合约，则使用 E​​RC1271 针对该智能合约验证签名，否则使用ECDSA.recover 
+
+    // 在 https://eips.ethereum.org/EIPS/eip-1271 中有签名合约的 isValidSignature 实现
+    // (妈的，最终还是用EOA进行签名和验证啊，将签名合约和一个EOA绑定在一起)
+    // 举个应用场景：链下订单簿的去中心化交易
+    // 之前的方式是EOA 签署订单，发送给 交易所智能合约 通过签名完成交易 （EOA ==签名、发送==> 交易所智能合约）
+    // 如果有中间商想做一个 中间商合约服务，链路变为：EOA ==发送数据(不签名)==> 中间商合约 ==签名===> 交易所智能合约
+    // 显然中间商合约现在是无法签名的，所以引入这个规范，相当于让合约也可以签名
+
     function isValidSignatureNow(
         address signer,
         bytes32 hash,
         bytes memory signature
     ) internal view returns (bool) {
+        // 1.先进行常规验证
         (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(hash, signature);
         if (error == ECDSA.RecoverError.NoError && recovered == signer) {
             return true;
         }
 
+        // 2.如果上面不通过，则将 signer 作为一个合约地址，调用其 isValidSignature 函数进行验证
         (bool success, bytes memory result) = signer.staticcall(
             abi.encodeWithSelector(IERC1271.isValidSignature.selector, hash, signature)
         );
